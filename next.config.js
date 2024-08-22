ab0ad5934ef1a0d9b8e6c5c89f2b836dff1c6aff
anthony Rodrigo <anrofer.28@gmail.com>
2024-08-22 18:43:52 -0500

    cambio en nextconfig

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    trailingSlash:true,
-  
-    async headers() {
-      return [
-        {
-          source: '/:all*(svg|jpg|png)',
-          locale: false,
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public, max-age=31536000, immutable',
-            }
-          ],
-        },
-      ]
-    },
-  
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  trailingSlash:true,
+//   output:"export",
+
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          }
+        ],
+      },
+    ]
+  },
+
+}
+
+module.exports = nextConfig
